c64a357e2e670ac02dbc312d9ace36b7698dda29
John Lester <retselnhoj.bodonal@gmail.com>
2025-01-02 23:27:49 +0800

    fix: just change the year 2025

--- src/home/Footer.jsx
+++ src/home/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className="py-[64px]" data-aos="fade-up" data-aos-duration="1000">
-        <p className="text-center font-semibold text-white">© 2024 John Lester Bodonal. All rights reserved.</p>
+        <p className="text-center font-semibold text-white">© 2025 John Lester Bodonal. All rights reserved.</p>
     </footer>
   )
 }
